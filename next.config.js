45f1405ec16924399fcc5c49abe61bf8b0233150
Nikunj <nikunjadm@gmail.com>
2026-06-14 23:13:27 +0530

    netlify config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
+  output: 'export', // This is required to generate the static files in the /out directory
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for static exports on Netlify
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
